aa25acc4d4497777d5f6da6f86b3bd7a9711b31c
Jason Cairns <jcai849@aucklanduni.ac.nz>
2020-10-19 13:16:12 +1300

    rpn

--- 4/getch.c
+++ 4/getch.c
@@ -2,8 +2,8 @@
 #include "calc.h"
 #define BUFSIZE	100
 
-char buf[BUFSIZE];	/* buffer for ungetch */
-int bufp = 0;		/* next free position in buf */
+static char buf[BUFSIZE];	/* buffer for ungetch */
+static int bufp = 0;		/* next free position in buf */
 
 int getch(void) /*get a (possibly pushed back) character */
 {
