f6c3b7aa73efc4d83649367713156ee485704add
Jason Cairns <jcai849@aucklanduni.ac.nz>
2020-10-23 16:46:43 +1300

    5-5

--- 5-5.c
+++ 5-5.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
+#include <string.h>
+#define iprint(x)	printf(#x ": %d\n", x)
+#define sprint(x)	printf(#x ": %s\n", x)
 
-int strcmp(char *s, char *t);
-void strcpy(char *s, char *t);
-void strcat(char *s, char *t);
+int strCmp(char *s, char *t);
+void strCpy(char *s, char *t);
+void strCat(char *s, char *t);
 
-char strncpy(char *s, char *ct, int n); /* copy at most n chars of string ct to
+void strNCpy(char *s, char *ct, int n); /* copy at most n chars of string ct to
 					   s; return s. Pad with '\0's if t has
 					   fewer than n characters */
-char strncat(char *s, char *ct, int n); /* concat at most n characters of
+void strNCat(char *s, char *ct, int n); /* concat at most n characters of
 					   string ct to string s, terminate s
 					   with '\0'; return s */
-int strncmp(char *cs, char *ct, int n); /* compare at most n characters of
+int strNCmp(char *cs, char *ct, int n); /* compare at most n characters of
 					   string cs to string ct; return <0 if
 					   cs<ct, 0 if cs==ct, or >0 if cs>ct
 					   */
@@ -22,16 +25,65 @@ int main()
 	     high[] = "ZZ";
 	char t[strlen(s)+2], u[strlen(s)+2];
 
-	strcpy(t, s);
-	strcpy(u, s);
+	sprint(s);
+	sprint(low);
+	sprint(high);
 
-	strcat(t, low);
-	strcat(u, high);
+	strNCpy(t, s, 9);
+	printf("strNCpy(t, s, 9); t: %s\n", t);
+	strNCpy(u, s, 9);
+	printf("strNCpy(u, s, 9); u: %s\n", u);
 
-	strcmp(s, s);
-	strcmp(t, s);
-	strcmp(u, s);
-	strcmp(t, u);
+	strNCat(t, low, 2);
+	printf("strNCat(t, low, 2); t: %s\n", t);
+	strNCat(u, high, 2);
+	printf("strNCat(u, high, 2); u: %s\n", u);
+
+	iprint(strNCmp(s, s, 9));
+	iprint(strNCmp(t, s, 9));
+	iprint(strNCmp(u, s, 9));
+	iprint(strNCmp(t, u, 9));
 
 	return 0;
+}
+
+void strCpy(char *s, char *t)
+{
+	while ((*s++ = *t++))
+		;
+}
+
+void strNCpy(char *s, char *ct, int n)
+{
+	while ((*s++ = *ct++) && n > 0)
+		n--;
+}
+
+void strCat(char *s, char *t)
+{
+	while (*++s != '\0');
+	while ((*s++ = *t++) != '\0');
+}
+
+void strNCat(char *s, char *ct, int n)
+{
+	while (*++s != '\0');
+	while ((*s++ = *ct++) != '\0' && n > 0)
+		n--;
+}
+
+int strCmp(char *s, char *t)
+{
+	for ( ; *s == *t; s++, t++)
+		if (*s == '\0')
+			return 0;
+	return *s - *t;
+}
+
+int strNCmp(char *cs, char *ct, int n)
+{
+	for ( ; *cs == *ct; cs++, ct++, n--)
+		if (*cs == '\0' || n == 0)
+			return 0;
+	return *cs - *ct;
 }
